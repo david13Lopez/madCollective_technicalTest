00914f9d1b23b6b98b1065ef0a492f6b70278a5a
David Lopez <dlopezru13@alumnes.ub.edu>
2023-01-18 17:24:34 +0100

    tests working

--- apiTest/cypress/e2e/Gifs_spec.cy.js
+++ apiTest/cypress/e2e/Gifs_spec.cy.js
@@ -1,14 +1,71 @@
 import data from '../fixtures/data.json' //added as variable to allow to change what you want to search without changing the test
 
-describe('Wikipedia searcher test', () => {
+describe('Gifs URL searchs', () => {
 
-    it('Should find what is setted in data fixtures', () => {
 
-        cy.visit("https://www.wikipedia.org")
-        cy.get('[id="search-input"]').type(data.searchParam) //get the input field by idanc type on it
-        cy.get('[data-jsl10n="search-input-button"]').click() //get the search button and click on it
-        cy.fixture('data').then((searchParam) => {
-            expect(data, 'the same data').to.deep.equal(searchParam) //asserting the data using the fixtures defined in data.json
-        })
+    it('Should search simple query ‘q’ parameter', () => {
+
+        cy.request('GET', `http://api.giphy.com/v1/gifs/search?q=${data.simpleSearchParam}&api_key=${data.apiKey}`).as('simpleQueryRequest');
+        cy.get('@simpleQueryRequest').then(response => {
+
+            expect(response.status).to.eq(200);
+            expect(response.body.data).to.have.lengthOf(50);
+            expect(response.body.pagination.total_count).to.eq(45045);
+            expect(response.body.data).to.have.lengthOf(response.body.pagination.count);
+
+        });
+    })
+
+    it('Should search composed query ‘q’ parameter', () => {
+
+        cy.request('GET', `http://api.giphy.com/v1/gifs/search?q=${data.multipleSearchParam}&api_key=${data.apiKey}`).as('multipleQueryRequest');
+        cy.get('@multipleQueryRequest').then(response => {
+
+            expect(response.status).to.eq(200);
+            expect(response.body.data).to.have.lengthOf(50);
+            expect(response.body.pagination.total_count).to.eq(58);
+            expect(response.body.data).to.have.lengthOf(response.body.pagination.count);
+
+        });
+    })
+
+    it('Should search simple query ‘q’ parameter and ‘limit’ parameter exceeding max.', () => {
+
+        cy.request('GET', `http://api.giphy.com/v1/gifs/search?q=${data.simpleSearchParam}&limit=${data.exceedLimitNumber}&api_key=${data.apiKey}`).as('zeroLimitQueryRequest');
+        cy.get('@zeroLimitQueryRequest').then(response => {
+
+            expect(response.status).to.eq(200);
+            expect(response.body.data).to.have.lengthOf(50);
+            expect(response.body.pagination.total_count).to.eq(45045);
+            expect(response.body.data).to.have.lengthOf(response.body.pagination.count);
+
+        });
+
+    })
+
+    it('Should search composed query ‘q’ parameter and ‘limit’ parameter exceeding max.', () => {
+
+        cy.request('GET', `http://api.giphy.com/v1/gifs/search?q=${data.multipleSearchParam}&limit=${data.exceedLimitNumber}&api_key=${data.apiKey}`).as('zeroLimitQueryRequest');
+        cy.get('@zeroLimitQueryRequest').then(response => {
+
+            expect(response.status).to.eq(200);
+            expect(response.body.data).to.have.lengthOf(50);
+            expect(response.body.pagination.total_count).to.eq(58);
+            expect(response.body.data).to.have.lengthOf(response.body.pagination.count);
+
+        });
+
+    })
+    it('Should search simple query ‘q’ parameter and ‘limit’ parameter set to 0 max', () => {
+
+        cy.request('GET', `http://api.giphy.com/v1/gifs/search?q=${data.simpleSearchParam}&limit=${data.zeroLimitNumber}&api_key=${data.apiKey}`).as('zeroLimitQueryRequest');
+        cy.get('@zeroLimitQueryRequest').then(response => {
+
+            expect(response.status).to.eq(200);
+            expect(response.body.data).to.have.lengthOf(0);
+            expect(response.body.pagination.total_count).to.eq(0);
+            expect(response.body.data).to.have.lengthOf(response.body.pagination.count);
+
+        });
     })
 })
